5bcbe0c47b037288c308fac3dbaeb11817e3f03e
Team Querators <teamquerators@gmail.com>
2019-04-10 10:24:30 +0800

    Third commit
    - Drinks pop up made

---
+++ PanelPopUp/Assets/PanelCloser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelCloser : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void ClosePanel()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(false);
+            Debug.Log("Panel closed");
+        }
+        else
+        {
+            Panel.SetActive(true);
+        }
+    }
+}
---
+++ PanelPopUp/Assets/PanelOpener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(true);
+            Debug.Log("Panel activated");
+        } else
+        {
+            Panel.SetActive(false);
+        }
+    }
+}
